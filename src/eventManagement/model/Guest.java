5645885e2a09d9be5cf86850dcb92d6cfc910313
Alexandru Rosca <alexandrurosca968@gmail.com>
2026-04-16 23:22:49 +0300

    Added Guest class, needs implementation for createAccount

--- src/eventManagement/model/Guest.java
+++ src/eventManagement/model/Guest.java
@@ -1,5 +1,10 @@
 package eventManagement.model;
 
 public class Guest {
+	public boolean createAccount(String username, String password) {
 
+		// TODO add method to create user object
+		
+		return true;
+	}
 }
