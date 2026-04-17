eadf46b57529ca86bb0c8d86e26b9fe97d99f458
Alexandru Rosca <alexandrurosca968@gmail.com>
2026-04-17 10:17:41 +0300

    Fixed constructor issue with Participant

--- src/eventManagement/model/Guest.java
+++ src/eventManagement/model/Guest.java
@@ -1,9 +1,9 @@
 package eventManagement.model;
 
 public class Guest {
-	public boolean createAccount(String username, String password) {
+	public boolean createAccount(String username, String password, String name, String surname) {
 
-        Participant newAccount = new Participant(username, password);
+        Participant newAccount = new Participant(username, password, name, surname);
         
         System.out.println("Account successfully created for: " + username);
         
